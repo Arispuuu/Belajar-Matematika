5f4678f7a4e5b50f57c87d0db7ccd687e793c229
Arispuuu <arispurnomo300@gmail.com>
2022-04-08 21:33:04 +0700

    tes sign in

--- app/src/main/java/com/c/belajarmatematika/activities/SignUpActivity.java
+++ app/src/main/java/com/c/belajarmatematika/activities/SignUpActivity.java
@@ -2,15 +2,26 @@ package com.c.belajarmatematika.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.c.belajarmatematika.R;
+import com.c.belajarmatematika.databinding.ActivitySignInBinding;
+import com.c.belajarmatematika.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
+    }
+
+    private void setListener() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }
