5b613a23a4452379ebabf5e05a36a7f8d4cc74d0
Arispuuu <arispurnomo300@gmail.com>
2022-04-08 19:09:06 +0700

    manifes

---
+++ app/src/main/java/com/c/belajarmatematika/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.c.belajarmatematika.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.c.belajarmatematika.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
